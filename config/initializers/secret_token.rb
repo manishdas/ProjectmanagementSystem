9ed9df6797b9ca65820371972a23273ce3f3dbc4
Manish <manish.das@sprout-technology.com>
2011-03-29 10:59:45 +0545

    Initial Structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManagementSystem::Application.config.secret_token = 'c325ee1fc84309625b162dfb1d4b87aaa026d907d8c3f4dd511c82196c68c5f104563c84fa78448bcfabe76d8fecedfe1ff5cff0d460a06b88e1c2e11c9a639a'
